9450054ea97cbe50d710aeb18b96f182dcbf9b21
satyam saurabh <ss9125rpvv@gmail.com>
2023-12-07 17:22:04 +0530

    removed build files

--- src/schema/typedefs/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResponseType = exports.EventType = void 0;
-var Event_js_1 = require("./Event.js");
-Object.defineProperty(exports, "EventType", { enumerable: true, get: function () { return Event_js_1.EventType; } });
-var Response_js_1 = require("./Response.js");
-Object.defineProperty(exports, "ResponseType", { enumerable: true, get: function () { return Response_js_1.ResponseType; } });
